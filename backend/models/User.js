24d11c333e2aa9fc5b5a97d51eb142d919e14400
parthasarathy s <146426915+parthasarathy0208@users.noreply.github.com>
2026-01-31 13:44:55 +0530

    Delete backend/models/User.js

--- backend/models/User.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
-  username: { type: String, unique: true },
-  passwordHash: String
-});
-module.exports = mongoose.model('User', UserSchema);
